2be675eb36595e13b5984ac7eda723cbe6f273e7
goforbroke <go.for.broke1006@gmail.com>
2017-08-30 09:51:59 +0300

    Initial commit

---
+++ src/com/gfb/golang_profiler_tool/ReloadAction.java
@@ -0,0 +1,35 @@
+package com.gfb.golang_profiler_tool;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+import java.io.IOException;
+
+public class ReloadAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        // TODO: insert action logic here
+
+        Project project = event.getData(PlatformDataKeys.PROJECT);
+        String scriptFilename = Messages.showInputDialog(project, "Filename", "Input script name", Messages.getQuestionIcon());
+//        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+
+        if (project == null || scriptFilename == null) {
+            return;
+        }
+
+        try {
+            final Runtime runtime = Runtime.getRuntime();
+
+//            runtime.exec("go test -cpuprofile cpu.prof -memprofile mem.prof -bench " + project.getBaseDir().getCanonicalPath());
+
+            runtime.exec("cd "+ project.getBaseDir().getCanonicalPath() + " && go build -o ./gfb-prof-build/" + scriptFilename.replace(".go",""));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+}
